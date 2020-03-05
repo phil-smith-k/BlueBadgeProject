388806ba989eee6cdc5abf072a3171b1aeb54224
Philip Smith <philip.smith.k@gmail.com>
2020-03-05 10:30:16 -0500

    added game details

---
+++ Basketball.Models/GameDetails.cs
@@ -0,0 +1,18 @@
+﻿using Basketball.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball.Models
+{
+    public class GameDetails
+    {
+        public string HomeTeamName { get; set; }
+        public string AwayTeamName { get; set; }
+        public DateTime Date { get; set; }
+        public int HomeTeamScore { get; set; }
+        public int AwayTeamScore { get; set; }
+    }
+}
