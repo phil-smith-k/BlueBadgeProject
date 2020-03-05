53fb8c5ef3cacdb940ad343234e1bf534bf5c97a
Philip Smith <philip.smith.k@gmail.com>
2020-03-05 10:36:01 -0500

    added game list

--- Basketball.Models/GameDetails.cs
+++ Basketball.Models/GameDetails.cs
@@ -9,10 +9,14 @@ namespace Basketball.Models
 {
     public class GameDetails
     {
+        public int GameId { get; set; }
         public string HomeTeamName { get; set; }
         public string AwayTeamName { get; set; }
+        public string Location { get; set; }
         public DateTime Date { get; set; }
         public int HomeTeamScore { get; set; }
         public int AwayTeamScore { get; set; }
+        public string WinningTeamName { get; set; }
+        public string LosingTeamName { get; set; }
     }
 }
