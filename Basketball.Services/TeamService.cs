ba79ae70a63960167f8308a11177cf2a5d8952b0
Scott Brooks <sjbrooks86@gmail.com>
2020-03-05 11:33:01 -0500

    Added team Services

---
+++ Basketball.Services/TeamService.cs
@@ -0,0 +1,68 @@
+﻿using Basketball.Data;
+using Basketball.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basketball.Services
+{
+    public class TeamService
+    {
+        public TeamDetails GetTeamById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Teams
+                        .Single(e => e.TeamId == id);
+                return
+                    new TeamDetails
+                    {
+                        Location = entity.Location,
+                        Name = entity.Name,
+                        ConferenceId = entity.ConferenceId
+                    };
+            }
+        }
+
+        public bool CreateNewTeam(CreateNewTeam model)
+        {
+            var entity =
+                new Team()
+                {
+                    Location = model.Location,
+                    Name = model.Name,
+                    ConferenceId = model.ConferenceId
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Teams.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<TeamDetails> GetTeams()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Teams
+                        .Select(T =>
+                                new TeamDetails
+                                {
+                                    Location = T.Location,
+                                    Name = T.Name,
+                                    ConferenceId = T.ConferenceId
+                                }
+                         );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
