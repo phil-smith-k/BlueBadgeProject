56a9d08d1063a418e088896c577ddcdbe4f4b523
Henry Decker <henryjdecker@gmail.com>
2020-03-05 15:05:30 -0500

    ControllerCommitHenry

---
+++ Basketball.API/Controllers/ConferenceController.cs
@@ -0,0 +1,65 @@
+﻿using Basketball.Models;
+using Basketball.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Basketball.API.Controllers
+{
+    public class ConferenceController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            ConferenceService conferenceService = CreateConferenceService();
+            var conference = conferenceService.GetConferences();
+            return Ok(conference);
+        }
+        public IHttpActionResult Post(CreateNewConference conference)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateConferenceService();
+
+            if (!service.CreateConference(conference))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private ConferenceService CreateConferenceService()
+        {
+            var conferenceService = new ConferenceService();
+            return conferenceService;
+        }
+        public IHttpActionResult Get(int id)
+        {
+            ConferenceService conferenceService = CreateConferenceService();
+            var conference = conferenceService.GetConferenceById(id);
+            return Ok(conference);
+        }
+        /*public IHttpActionResult Put(ConferenceEdit conference)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateConferenceService();
+
+            if (!service.UpdateConference(conference))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateConferenceService();
+
+            if (!service.DeleteConference(id))
+                return InternalServerError();
+
+            return Ok();
+        }*/
+    }
+}
---
+++ Basketball.API/Controllers/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using Basketball.Models;
+using Basketball.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Basketball.API.Controllers
+{
+    public class PlayerController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            PlayerService playerService = CreatePlayerService();
+            var players = playerService.GetPlayers();
+            return Ok(players);
+        }
+        public IHttpActionResult Post(CreateNewPlayer player)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePlayerService();
+
+            if (!service.CreatePlayer(player))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private PlayerService CreatePlayerService()
+        {
+            var playerService = new PlayerService();
+            return playerService;
+        }
+        public IHttpActionResult Get(int id)
+        {
+            PlayerService playerService = CreatePlayerService();
+            var player = playerService.GetPlayerById(id);
+            return Ok(player);
+        }
+        /*public IHttpActionResult Put(PlayerEdit player)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdatePlayer(player))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePlayerService();
+
+            if (!service.DeletePlayer(id))
+                return InternalServerError();
+
+            return Ok();
+        }*/
+    }
+}
